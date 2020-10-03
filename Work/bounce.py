a27c8abcc16b5f51eeca070e0f75a1e0bd6e551a
balaji-s <balajisundar.s@gmail.com>
2020-10-03 20:59:54 +0530

    added solution for bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,13 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100
+no_of_bounce = 10
+bounced_back = 3/5
+initial_bounce = 1
+
+while initial_bounce <= no_of_bounce:
+    bounced = round(height * bounced_back, 4)
+    print(bounced)
+    height = bounced
+    initial_bounce += 1
