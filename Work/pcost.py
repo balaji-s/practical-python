082c948dc641222a1dc7f1f8c403438f28e167a4
balaji-s <balajisundar.s@gmail.com>
2020-10-04 10:04:05 +0530

    rewrote with csv module

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,15 @@
 # pcost.py
 #
 # Exercise 1.27
+import csv
+def portfolio_cost(filename):
+    with open(filename, 'rt') as file:
+        portfolio = csv.reader(file)
+        next(portfolio) #skip the header
+        sum = 0.0
+        for company in portfolio:
+            sum += int(company[1]) * float(company[2])
+    return sum
+
+sum = portfolio_cost('Data/portfolio.csv')
+print('Total cost:', sum)
